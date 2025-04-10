852fd48dc74666e0f4819df5f77095f9131c1d20
Paulo Fonseca <pfonseca.arq@gmail.com>
2025-04-10 09:39:17 -0300

    css changes Header

--- mywebpage/src/Components/Header/Header.jsx
+++ mywebpage/src/Components/Header/Header.jsx
@@ -3,22 +3,28 @@ import './Header.css'
 
 function Header() {
   return (
-    <div className='header-container'>
+    <div className="header-container">
       <div>
-          <p className='header-name'>Paulo Fonseca</p>
-          <p className='header-profession'>Desenvolvedor Front-End</p>
+        <p className="header-name">Paulo Fonseca</p>
+        <p className="header-profession">Desenvolvedor Front-End</p>
       </div>
 
-      <div className='header-text'>
-          <p>Acredito que a colaboração e o aprendizado contínuo são a chave 
-            para o sucesso no desenvolvimento Front-End. Sou Paulo Fonseca,
-            um desenvolvedor apaixonado por tecnologia. 
-            Adoro trabalhar em equipe, compartilhar conhecimento e construir
-            soluções que impactem positivamente a vida das pessoas.
-          </p>
+      <div className='header-intro'>
+        <p>
+          “Acredito que a colaboração e o aprendizado contínuo são a chave para
+          o sucesso no desenvolvimento Front-End.”
+        </p>
+      </div>
+
+      <div className="header-text">
+        <p>
+          Um desenvolvedor apaixonado por tecnologia. Adoro
+          trabalhar em equipe, compartilhar conhecimento e construir soluções
+          que impactem positivamente a vida das pessoas.
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
 export default Header
